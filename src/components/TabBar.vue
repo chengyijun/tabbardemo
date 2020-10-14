b5f62cd1545841d837c39074f7fba73d3195e012
chengyijun <cyjmmy@foxmail.com>
2020-10-14 21:43:13 +0800

    初始化

---
+++ src/components/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tabbar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBar"
+    }
+</script>
+
+<style scoped>
+  .tabbar {
+    display: flex;
+    background-color: #f0f0f0;
+    box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
+
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+  }
+</style>
---
+++ src/components/TabBarItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tabbar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-img"></slot></div>
+    <div v-else><slot name="item-img-active"></slot></div>
+    <div v-if="isActive" v-bind:class="{active:isActive}"><slot name="item-text"></slot></div>
+    <div v-else><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBarItem",
+        props: {
+          path: String
+        },
+        data() {
+          return {
+            // isActive: false
+          }
+        },
+      computed: {
+        isActive:{
+          get() {
+            return this.$route.path.indexOf(this.path) !== -1
+          },
+          set(val){
+
+          }
+        }
+      },
+      methods: {
+        itemClick() {
+          // 如果当前页面不是激活的就跳转，反之不跳转
+          if(this.$route.path.indexOf(this.path) === -1){
+            this.$router.replace(this.path)
+          }
+
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .tabbar-item {
+    flex: 1;
+    height: 49px;
+    font-size: 14px;
+    text-align: center;
+
+  }
+
+  .tabbar-item img {
+
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+  .active{
+    color: #19fa28;
+  }
+</style>
