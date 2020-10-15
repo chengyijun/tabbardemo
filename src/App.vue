3edcabdd6fc02ad2604edbcf7a5ce00fb7ac559a
chengyijun <cyjmmy@foxmail.com>
2020-10-15 08:53:45 +0800

    tabbar活动状态文本颜色动态传入

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <router-view></router-view>
     <tab-bar>
       <tab-bar-item path="/home">
-          <img slot="item-img" src="./assets/img/主页.svg" alt="">
-          <img slot="item-img-active" src="./assets/img/主页a.svg" alt="">
-          <div slot="item-text">首页</div>
+        <img slot="item-img" src="./assets/img/主页.svg" alt="">
+        <img slot="item-img-active" src="./assets/img/主页a.svg" alt="">
+        <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item path="/category">
         <img slot="item-img" src="./assets/img/分类.svg" alt="">
@@ -13,15 +13,15 @@
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item path="/shopcart">
-      <img slot="item-img" src="./assets/img/购物车.svg" alt="">
-      <img slot="item-img-active" src="./assets/img/购物车a.svg" alt="">
-      <div slot="item-text">购物车</div>
-    </tab-bar-item>
+        <img slot="item-img" src="./assets/img/购物车.svg" alt="">
+        <img slot="item-img-active" src="./assets/img/购物车a.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
       <tab-bar-item path="profile">
-      <img slot="item-img" src="./assets/img/我的.svg" alt="">
-      <img slot="item-img-active" src="./assets/img/我的a.svg" alt="">
-      <div slot="item-text">我的</div>
-    </tab-bar-item>
+        <img slot="item-img" src="./assets/img/我的.svg" alt="">
+        <img slot="item-img-active" src="./assets/img/我的a.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
     </tab-bar>
   </div>
 </template>
@@ -29,6 +29,7 @@
 <script>
   import TabBar from "./components/TabBar";
   import TabBarItem from "./components/TabBarItem";
+
   export default {
     name: 'App',
     components: {
@@ -41,8 +42,6 @@
 
 <style>
   @import "assets/css/base.css";
-
-
 
 
 </style>
--- src/components/TabBarItem.vue
+++ src/components/TabBarItem.vue
@@ -1,43 +1,58 @@
 <template>
   <div class="tabbar-item" @click="itemClick">
-    <div v-if="!isActive"><slot name="item-img"></slot></div>
-    <div v-else><slot name="item-img-active"></slot></div>
-    <div v-if="isActive" v-bind:class="{active:isActive}"><slot name="item-text"></slot></div>
-    <div v-else><slot name="item-text"></slot></div>
+    <div v-if="!isActive">
+      <slot name="item-img"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-img-active"></slot>
+    </div>
+    <div v-if="isActive" v-bind:class="{active:isActive}" v-bind:style="itemTextColor">
+      <slot name="item-text"></slot>
+    </div>
+    <div v-else v-bind:style="itemTextColor">
+      <slot name="item-text"></slot>
+    </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "TabBarItem",
-        props: {
-          path: String
+  export default {
+    name: "TabBarItem",
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: '#19fa28'
+      }
+    },
+    data() {
+      return {
+        // isActive: false
+      }
+    },
+    computed: {
+      isActive: {
+        get() {
+          return this.$route.path.indexOf(this.path) !== -1
         },
-        data() {
-          return {
-            // isActive: false
-          }
-        },
-      computed: {
-        isActive:{
-          get() {
-            return this.$route.path.indexOf(this.path) !== -1
-          },
-          set(val){
+        set(val) {
 
-          }
         }
       },
-      methods: {
-        itemClick() {
-          // 如果当前页面不是激活的就跳转，反之不跳转
-          if(this.$route.path.indexOf(this.path) === -1){
-            this.$router.replace(this.path)
-          }
-
+      itemTextColor() {
+        return this.isActive ? {color: this.activeColor} : {}
+      }
+    },
+    methods: {
+      itemClick() {
+        // 如果当前页面不是激活的就跳转，反之不跳转
+        if (this.$route.path.indexOf(this.path) === -1) {
+          this.$router.replace(this.path)
         }
+
       }
     }
+  }
 </script>
 
 <style scoped>
@@ -57,7 +72,6 @@
     vertical-align: middle;
     margin-bottom: 2px;
   }
-  .active{
-    color: #19fa28;
-  }
+
+
 </style>
